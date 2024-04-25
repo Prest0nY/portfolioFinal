04e04a2912d84e190c9b6d76222cf61c375b2903
Prest0nY <75341414+Prest0nY@users.noreply.github.com>
2024-04-24 20:26:45 -0700

    Added images and rearranged sections
    moved everything from index.html to app.js

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -3,21 +3,54 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      
+      <div className="App">
+        
+          
+        <h1>Preston Yong</h1>
+
+        <p>I am a freshman at Cal Poly Pomona, looking forward to studying computer science. Here's some of my work so far:</p>
+
+        <div className="gallery">
+          <div>
+            <h3>StarField Animation</h3>
+            <img src="https://images.pexels.com/photos/998641/pexels-photo-998641.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+          </div>
+
+          <div>
+            <h3>Volleyball Rotations</h3>
+            <img src="https://images.pexels.com/photos/2444852/pexels-photo-2444852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+          </div>
+        </div>
+
+        <div>
+          <h3>Fishtank Animation</h3>
+          <img src="https://images.pexels.com/photos/11267182/pexels-photo-11267182.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+          <h3></h3>
+        </div>
+
+
+        
+        
+      </div>
+
+
+
+
+      <div>
+      <h3>Contact Me!</h3>
+        <ul>
+          <li>email</li>
+          <li>number</li>
+          <li>linkedin</li>
+        </ul>
+      </div>
+
+
+
+
+
     </div>
   );
 }
