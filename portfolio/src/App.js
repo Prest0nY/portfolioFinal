6c81989bf76297c7e44e3e5a6f5e31ea9ca97535
Prest0nY <75341414+Prest0nY@users.noreply.github.com>
2024-04-26 14:17:42 -0700

    images and formated contacts
    I made the images clickable links and formatted the contact options to be displayed next to each other.

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -15,18 +15,27 @@ function App() {
         <div className="gallery">
           <div>
             <h3>StarField Animation</h3>
-            <img src="https://images.pexels.com/photos/998641/pexels-photo-998641.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+
+            <a href="https://www.w3schools.com/tags/att_a_target.asp" target="_blank">
+              <img src="https://images.pexels.com/photos/998641/pexels-photo-998641.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+            </a>
           </div>
 
           <div>
             <h3>Volleyball Rotations</h3>
-            <img src="https://images.pexels.com/photos/2444852/pexels-photo-2444852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+
+            <a href="https://www.github.com" target="_blank">
+              <img src="https://images.pexels.com/photos/2444852/pexels-photo-2444852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+            </a>
+
           </div>
         </div>
 
         <div>
           <h3>Fishtank Animation</h3>
-          <img src="https://images.pexels.com/photos/11267182/pexels-photo-11267182.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+          <a href="https://www.google.com/maps" target="_blank">
+            <img src="https://images.pexels.com/photos/11267182/pexels-photo-11267182.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+          </a>
           <h3></h3>
         </div>
 
@@ -39,12 +48,14 @@ function App() {
 
 
       <div>
-      <h3>Contact Me!</h3>
-        <ul>
-          <li>email</li>
-          <li>number</li>
-          <li>linkedin</li>
-        </ul>
+        <h3 className="App">Contact Me!</h3>
+
+        <div className="gallery">
+          <h5>Email</h5>
+          <h5>Phone Number</h5>
+          <h5>Linkedin</h5>
+        </div>
+        
       </div>
 
 
