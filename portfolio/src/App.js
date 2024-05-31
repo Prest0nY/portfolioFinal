9fe1a9682e474e33cdc88e19cc4df0fbf536ec13
Prest0nY <75341414+Prest0nY@users.noreply.github.com>
2024-05-31 09:28:58 -0700

    switched fishtank for amazon and edited title

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import './index.css'
+import amazonBox from "./amazonBox.jpg"
 
 function App() {
   return (
@@ -27,18 +28,20 @@ function App() {
 
           <div className='gallery'>
             
-            <a href=""><img className='max-w-lg' src="https://images.pexels.com/photos/998641/pexels-photo-998641.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/></a>
+            <a href="" target="_blank"><img className='max-w-lg' src="https://images.pexels.com/photos/998641/pexels-photo-998641.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/></a>
             <a href="https://prest0ny.github.io/VolleyballRotations/" target="_blank"><img className='max-w-lg' src="https://images.pexels.com/photos/2444852/pexels-photo-2444852.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/></a>
             
           </div>
 
           <div className='gallery'>
-            <h3 className='text-4xl pt-36'>Fishtank Animation</h3>
+            <h3 className='text-4xl pt-36'>Amazon Replica (not completely working currently)</h3>
           </div>
 
           <div className='gallery'>
             
-            <img className='max-w-lg pb-16' src="https://images.pexels.com/photos/11267182/pexels-photo-11267182.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"/>
+            <a href="https://prest0ny.github.io/AmazonReplica/" target="_blank">
+              <img className='max-w-lg pb-16' src={amazonBox}/>
+            </a>
             
           </div>
 
